c4b60dcb64052d87b8eb5b71c57ad78c19d7e21d
rzuckerm <ron.zuckerman@adtran.com>
2025-07-10 16:47:34 -0500

    Add Flower Field in C++

---
+++ flower-field/flower_field.cpp
@@ -0,0 +1,23 @@
+#include "flower_field.h"
+
+namespace flower_field {
+static int directions[][2]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+std::vector<std::string> annotate(const std::vector<std::string> &input) {
+    std::vector<std::string> output(input);
+    for (int i = 0, nr = int(output.size()); i < nr; i++) {
+        for (int j = 0, nc = int(output[i].size()); j < nc; j++) {
+            char count{'0'};
+            if (output[i][j] == ' ') {
+                for (auto d : directions) {
+                    int r{i + d[0]}, c{j + d[1]};
+                    count += (r >= 0 && r < nr && c >= 0 && c < nc && output[r][c] == '*');
+                    output[i][j] = (count == '0') ? ' ' : count;
+                }
+            }
+        }
+    }
+
+    return output;
+}
+}  // namespace flower_field
---
+++ flower-field/flower_field.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace flower_field {
+std::vector<std::string> annotate(const std::vector<std::string> &input);
+}  // namespace flower_field
