26c43b54f0fe699c700f1e82339442f76a5539dd
rzuckerm <rzdulc@gmail.com>
2026-06-09 17:27:17 -0500

    Upate Two-Fer in C++

--- two-fer/two_fer.cpp
+++ two-fer/two_fer.cpp
@@ -1,9 +1,5 @@
 #include "two_fer.h"
 
-namespace two_fer
-{
-std::string two_fer(const std::string &name)
-{
-    return "One for " + (name.empty() ? "you" : name) + ", one for me.";
-}
-} // namespace two_fer
+namespace two_fer {
+std::string two_fer(const std::string& name) { return "One for " + (name.empty() ? "you" : name) + ", one for me."; }
+}  // namespace two_fer
--- two-fer/two_fer.h
+++ two-fer/two_fer.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 
-namespace two_fer
-{
-std::string two_fer(const std::string &name = "");
-} // namespace two_fer
+namespace two_fer {
+std::string two_fer(const std::string& name = "");
+}  // namespace two_fer
